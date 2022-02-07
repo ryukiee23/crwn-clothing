183d7c3e6a887936077dea228db7d2ba3c45dab3
arpit23 <arpitsharma20014@gmail.com>
2022-02-07 13:48:17 +0530

    Second Changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './HomePage.component';
+import HomePage from './pages/HomePage/HomePage.component';
 
 function App() {
   return (
