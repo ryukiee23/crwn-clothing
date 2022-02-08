87982b7aaf0dee29814a49d5391cd917414f5a90
arpit23 <arpitsharma20014@gmail.com>
2022-02-08 20:19:01 +0530

    Shop Page added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import './App.css';
 import {Route} from 'react-router-dom';
 
 import HomePage from './pages/HomePage/HomePage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
     <Route exact path='/' component={HomePage}></Route>
-    <Route  path='/shop/hats' component={HatsPage}></Route>
+    <Route  path='/shop' component={ShopPage}></Route>
     </div>
   );
 }
