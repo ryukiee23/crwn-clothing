d5e190660ecb6232d3aaa0aee7a1de9ad24c41b6
arpit23 <arpitsharma20014@gmail.com>
2022-02-08 12:39:48 +0530

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
 
 import HomePage from './pages/HomePage/HomePage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-     <HomePage / >
+    <Route exact path='/' component={HomePage}></Route>
+    <Route  path='/shop/hats' component={HatsPage}></Route>
     </div>
   );
 }
