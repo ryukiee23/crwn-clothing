a4adccec16f16fcc9cdadfdf16f9defe2159b037
arpit23 <arpitsharma20014@gmail.com>
2022-02-08 20:44:43 +0530

    Header added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './App.css';
-import {Route} from 'react-router-dom';
+import {Route ,Switch} from 'react-router-dom';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/HomePage/HomePage.component';
 import ShopPage from './pages/shop/shop.component';
 
 
 function App() {
   return (
+
     <div>
-    <Route exact path='/' component={HomePage}></Route>
-    <Route  path='/shop' component={ShopPage}></Route>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route path='/shop' component={ShopPage}></Route>
+      </Switch>
+
     </div>
   );
 }
